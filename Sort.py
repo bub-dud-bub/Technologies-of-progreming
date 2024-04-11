5c174365b87033fa192abab7f984ca85c3f25f89
Real Serafim <61107480+bub-dud-bub@users.noreply.github.com>
2024-04-12 03:19:15 +0500

    Update Sort.py

--- Sort.py
+++ Sort.py
@@ -1,5 +1,7 @@
 class Sort():
-    def Selection(list):
+    def __init__(self):
+        pass
+    def Selection(self, list):
         i = 0
         j = 1
         r = len(list)
@@ -8,39 +10,50 @@ class Sort():
             while(j<r):
                 if (list[j] < list[i]):
                     list[i], list[j] = list[j], list[i]
+                    Data().data(list)
                 j+=1
             i+=1
         return list
 
-    def Merge(list):
+    def Merge(self, list):
         if len(list)==1:
-            return list[0]
+            Data().data(list)
+            return list
         list1, list2, i = [], [], 0
         while (i<len(list)//2):
              list1.append(list[i])
              i+=1
-        a = Sort.Merge(list1)
+        a = self.Merge(list1)
         while (i<len(list)):
              list2.append(list[i])
              i+=1
-        b = Sort.Merge(list2)
-        i, j = 0, 0
-        c = [a[i]]
+        b = self.Merge(list2)
+        i, c = 0, []
         while (i<(len(a)+len(b))):
-            while(j<len(b)):
-                if a[j]<c[i]:
-                    c[i] = a[j]
-                    a.remove(a[j])
-                    j=0
-                    break
-                j+=1
-            while(j<len(b)):
-                if b[j]<c[i]:
-                    c[i] = b[j]
-                    b.remove(b[j])
-                    break
-                j+=1
+            if (len(a)==0):
+                c+=b
+                break
+            elif (len(b)==0):
+                c+=a
+                break
+            if a[0]<=b[0]:
+                c.append(a[0])
+                a.remove(a[0])
+            else:
+                c.append(b[0])
+                b.remove(b[0])
+        Data().data(c)
         return c
+
+class Data(Sort):
+    def data(self, list):
+        var = list
+        return Vision().vision(var)
+
+class Vision(Data):
+    def vision(self, list):
+        print(list)
+
 list = [9, 5, 6, 9, 8, 4, 4]
-a = Sort.Merge(list)
-print(a)
+#Sort().Selection(list)
+Sort().Merge(list)
